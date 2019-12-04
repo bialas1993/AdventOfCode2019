bc41b18c27ab0d6bbb641e2267cbfdb5de707b2b
Firstname Lastname <foo@bar.com>
2019-12-04 21:40:39 +0100

    solution part 2

--- bin/day04/day04.dart
+++ bin/day04/day04.dart
@@ -1,25 +1,76 @@
+import 'dart:io';
+
 main(List<String> arguments) async {
-  var input = '245182-790572';
+
+  solutionA('245182-790572');
+  solutionB('245182-790572');
+}
+
+solutionA (String input) {
   var range = input.split('-').map(int.parse).toList();
   var maches = 0;
   print('Range: $range');
 
   for (var i=range[0]; i <= range[1]; i++) {
-    if (validate(i)) {
+    if (validate(i.toString())) {
       maches++;
     }
   }
 
-  print('Maches: $maches');
+  print('Part 1 Maches: $maches');
 }
 
-bool validate(int n) {
-  var s = n.toString();
-  var lastChar = 0;
+solutionB (String input) {
+  var range = input.split('-').map(int.parse).toList();
+  var maches = 0;
+  print('Range: $range');
+
+  for (var i=range[0]; i <= range[1]; i++) {
+    if (isIncreasing(i.toString()) && isAdjacentDouble(i.toString())) {
+      maches++;
+    }
+  }
+
+  print('Part 2 Maches: $maches');
+}
+
+bool isIncreasing(String s) {
+  var lastChar = s[0].codeUnitAt(0);
+
+  for (var i=1; i<s.length; i++) {
+      var char = s[i].codeUnitAt(0);
+
+      if (!(lastChar <= char)) {
+        return false;
+      }
+
+      lastChar = char;
+  }
+
+  return true;
+}
+
+bool isAdjacentDouble(String s) {
+  int seq = 1;
+  for (var l = 1; l < s.length; l++) {
+    if (s[l] == s[l - 1]) {
+      seq++;
+    } else if (seq == 2) {
+      return true;
+    } else {
+      seq = 1;
+    }
+  }
+
+  return seq == 2;
+}
+
+bool validate(String s) {
+  var lastChar = s[0].codeUnitAt(0);
   var doubleNum = false;
 
-  for (var i=0; i<s.length; i++) {
-      var char =  s[i].codeUnitAt(0);
+  for (var i=1; i<s.length; i++) {
+      var char = s[i].codeUnitAt(0);
 
       if (lastChar <= char) {
         if (lastChar == char) {
